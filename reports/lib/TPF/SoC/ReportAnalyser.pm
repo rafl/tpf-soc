c163e51265f35a6acecac91ff90706bb4f75b6d4
Florian Ragwitz <rafl@debian.org>
2011-06-03 11:42:59 +0200

    Collect events while analysing

--- reports/lib/TPF/SoC/ReportAnalyser.pm
+++ reports/lib/TPF/SoC/ReportAnalyser.pm
@@ -2,9 +2,15 @@ package TPF::SoC::ReportAnalyser;
 
 use Moose;
 use syntax 'method';
+use DateTime::Span;
 use DateTime::Duration;
 use MooseX::Types::DateTime DateTime => { -as => 'DateTimeType' }, 'Duration';
 use TPF::SoC::Types 'DateTimeSpan';
+use aliased 'TPF::SoC::ReportingPeriod';
+use aliased 'TPF::SoC::ReportingPeriod::Event::Timely', 'TimelyReportEvent';
+use aliased 'TPF::SoC::ReportingPeriod::Event::Bonus', 'BonusReportEvent';
+use aliased 'TPF::SoC::ReportingPeriod::Event::MissedDeadline', 'MissedDeadlineEvent';
+use aliased 'TPF::SoC::ReportingPeriod::Event::MissedExpectedDeadline', 'MissedExpectedDeadlineEvent';
 use namespace::autoclean;
 
 has reporting_period => (
@@ -42,32 +48,48 @@ method analyse (@reports) {
 
     warn $reports[0]->student->nick;
 
+    my @reporting_periods;
+
     while ($last_reporting_start <= $now && $next_reporting_start < $self->reporting_period_end) {
         warn $next_reporting_deadline;
+
+        my @events;
 
         if ($next_expected_reporting_date > $next_reporting_deadline) {
             die "$next_expected_reporting_date > $next_reporting_deadline";
         }
 
-        my $reports_consumed = 0;
+        my @reports_in_period;
         while (@reports && $reports[0]->date < $next_reporting_deadline) {
-            my $report = shift @reports;
-            $reports_consumed++;
+            push @reports_in_period, shift @reports;
+        }
 
+        for my $report (@reports_in_period) {
             $next_expected_reporting_date = $self->next_reporting_date(
                 $report->date->clone->truncate(to => 'day'),
             ) + DateTime::Duration->new(days => 1);
+
+            my $event_class = @reports_in_period > 1
+                ? BonusReportEvent : TimelyReportEvent;
+
+            push @events, $event_class->new({
+                date               => $report->date,
+                report             => $report,
+                expected_next_date => $next_expected_reporting_date,
+            });
 
             warn "report before deadline (" . $report->date . ")";
             warn "(next expected at $next_expected_reporting_date)";
         }
 
-        if ($reports_consumed > 1) {
-            warn "bonus report!";
-        }
-        elsif (!$reports_consumed) {
+        unless (@reports_in_period) {
             if ($next_reporting_deadline > $now) {
                 if ($next_expected_reporting_date > $now) {
+                    push @events, MissedExpectedDeadlineEvent->new({
+                        date          => $now,
+                        expected_date => $next_expected_reporting_date,
+                    });
+
                     warn "no report yet for week until " . $next_reporting_deadline
                         . " even though it was expected to arrive before " . $next_expected_reporting_date;
                 }
@@ -80,10 +102,23 @@ method analyse (@reports) {
                     $next_expected_reporting_date,
                 );
 
+                push @events, MissedDeadlineEvent->new({
+                    date               => $next_reporting_deadline,
+                    expected_next_date => $next_expected_reporting_date,
+                });
+
                 warn "missed deadline for week until " . $next_reporting_deadline;
                 warn "(next expected at $next_expected_reporting_date)";
             }
         }
+
+        push @reporting_periods, ReportingPeriod->new({
+            events => \@events,
+            span   => DateTime::Span->new(
+                start => $last_reporting_start,
+                end   => $next_reporting_deadline,
+            ),
+        });
 
         $last_reporting_start = $next_reporting_start;
         $next_reporting_start = $self->next_reporting_date($last_reporting_start);
@@ -91,6 +126,8 @@ method analyse (@reports) {
     }
 
     print "\n";
+
+    return \@reporting_periods;
 }
 
 __PACKAGE__->meta->make_immutable;
