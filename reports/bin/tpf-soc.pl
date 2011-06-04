fe412c0387b6ab5fb1c145cd99bbcab1478b9403
Florian Ragwitz <rafl@debian.org>
2011-06-04 18:44:33 +0200

    Add an MX::App::Cmd app

---
+++ reports/bin/tpf-soc.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use TPF::SoC::Cmd;
+
+exit TPF::SoC::Cmd->run;
---
+++ reports/lib/TPF/SoC/Cmd.pm
@@ -0,0 +1,10 @@
+package TPF::SoC::Cmd;
+
+use Moose;
+use namespace::autoclean;
+
+extends 'MooseX::App::Cmd';
+
+__PACKAGE__->meta->make_immutable;
+
+1;
